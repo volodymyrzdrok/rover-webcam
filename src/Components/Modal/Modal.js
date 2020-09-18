1c9cabc36ca1de86f9917a4b70e0d25107dbe4fb
volodymyrzdrok <volodymyrzdrok44@gmail.com>
2020-09-18 16:44:15 +0300

    add-useEftect

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import './Modal.css';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.henleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.henleKeyDown);
-  }
-  henleKeyDown = e => {
-    e.code === 'Escape' && this.props.toggleModal();
-  };
-  render() {
-    return (
-      <CSSTransition
-        in={this.props.showModal}
-        timeout={300}
-        classNames="alert"
-        unmountOnExit
-      >
-        <div className="Overlay" onClick={this.props.toggleModal}>
-          <div className="Modal">
-            <img
-              src={this.props.largeImage}
-              alt="modalImage"
-              className="modalImage"
-            />
-          </div>
+const Modal = ({ toggleModal, largeImage, showModal }) => {
+  useEffect(() => {
+    const henleKeyDown = e => {
+      e.code === 'Escape' && toggleModal();
+    };
+
+    window.addEventListener('keydown', henleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', henleKeyDown);
+    };
+  }, [toggleModal]);
+
+  return (
+    <CSSTransition
+      in={showModal}
+      timeout={300}
+      classNames="alert"
+      unmountOnExit
+    >
+      <div className="Overlay" onClick={toggleModal}>
+        <div className="Modal">
+          <img src={largeImage} alt="modalImage" className="modalImage" />
         </div>
-      </CSSTransition>
-    );
-  }
-}
+      </div>
+    </CSSTransition>
+  );
+};
+
+export default Modal;
